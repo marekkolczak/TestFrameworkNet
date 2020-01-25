d1dddad472f8abf28aca1e3cd2d0d11934da5a90
Marek <marekkolczak4@yahoo.co.uk>
2020-01-25 10:31:13 +0100

    first commit

---
+++ Tests/TestFramework/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework
+{
+    public class Class1
+    {
+        string a = "Fix";
+
+        public string A { get => a; set => a = value; }
+    }
+}
